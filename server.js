76c55c1d6c8823e635531ec251532267ff89f6b1
OktayCopurlu <selimhalim12@gmail.com>
2021-06-21 02:17:26 +0200

    m

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger/swagger.json");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(cookieParser());
